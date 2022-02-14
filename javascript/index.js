982d81f657e2cf0bb57b3da338a4bdc13ca0eff8
Amrit Sidhu <amritsidhu812@gmail.com>
2022-02-14 14:18:03 +0530

    mobilesetting

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,25 @@
 console.log("hello Amrit new  starts here..........");
 
 //toggle mobile navbar
-let hamburger = document.querySelector(".hamburger");
-let navBarMobile = document.querySelector(".navBarMobile");
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("is-active");
-  navBarMobile.classList.toggle("show-navBarMobile");
-  console.log("working");
-});
+(function toggleMobileNavBar() {
+  let hamburger = document.querySelector(".hamburger");
+  let navBarMobile = document.querySelector(".navBarMobile");
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active");
+    navBarMobile.classList.toggle("show-navBarMobile");
+  });
+})();
+
+//function to close mobile menu after click on any menu link
+(function closeMobileMenu() {
+  let hamburger = document.querySelector(".hamburger");
+  let navBarMobile = document.querySelector(".navBarMobile");
+  let navLinksMobile = document.querySelectorAll(".navLinksMobile li a");
+  navLinksMobile.forEach(function (navLink) {
+    navLink.addEventListener("click", function () {
+      console.log(this);
+      hamburger.classList.toggle("is-active");
+      navBarMobile.classList.toggle("show-navBarMobile");
+    });
+  });
+})();
