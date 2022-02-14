3324ecf602f738fd272c873326b91fc3ed030812
Amrit Sidhu <amritsidhu812@gmail.com>
2022-02-14 10:44:50 +0530

    asdfgh

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,9 @@ console.log("hello Amrit new  starts here..........");
 
 //toggle mobile navbar
 let hamburger = document.querySelector(".hamburger");
+let navBarMobile = document.querySelector(".navBarMobile");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("is-active");
+  navBarMobile.classList.toggle("show-navBarMobile");
   console.log("working");
 });
